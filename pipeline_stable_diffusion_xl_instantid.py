977a8a527e7daec3e9b8f50089152c6a9933bf19
ResearcherXman <xhs.research@gmail.com>
2024-01-22 19:42:51 +0800

    update

--- pipeline_stable_diffusion_xl_instantid.py
+++ pipeline_stable_diffusion_xl_instantid.py
@@ -22,6 +22,7 @@ import numpy as np
 import PIL.Image
 import torch
 import torch.nn.functional as F
+from transformers import CLIPTokenizer
 
 from diffusers.image_processor import PipelineImageInput
 
@@ -40,7 +41,6 @@ from diffusers.pipelines.controlnet.multicontrolnet import MultiControlNetModel
 from diffusers.utils.import_utils import is_xformers_available
 
 from ip_adapter.resampler import Resampler
-from ip_adapter.utils import is_torch2_available
 
 from ip_adapter.attention_processor import AttnProcessor, IPAttnProcessor
 from ip_adapter.attention_processor import region_control
